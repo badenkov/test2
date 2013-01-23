53f812de298a68a01a0b6b0cf8f190eeffab5031
Alexey Badenkov <alexey.badenkov@gmail.com>
2013-01-23 22:17:55 +0400

    сделал отправку сообщений пользователю о новом сообщении

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
+# encoding=utf-8
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def new_message_notification(user, message)
     @user = user
     @message = message
-    logger.debug Сообщение .user.email
     mail(to: user.email, subject: 'Поступило новое сообщение')
   end
 end
